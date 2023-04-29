c566cb9519eb9bfaa21d8a61b94ddf72a1dfc01b
websolucionOrg <131605739+websolucionOrg@users.noreply.github.com>
2023-04-29 11:38:20 +0000

    dssddsdss

---
+++ components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+import  { BsDisc } from 'react-icons/bs'
+
+const Header = () => {
+  return (
+    <header className='header'>
+        <div className='container'>
+            <Link href="/" passHref > <BsDisc /> </Link>
+            <nav>
+               <ul>
+                 <li><Link href="/" passHref > Home </Link></li>
+                 <li><Link href="/" passHref > Artistas </Link></li>
+                 <li><Link href="/" passHref > Musicas </Link></li>
+               </ul>
+            </nav>          
+        </div>
+
+    </header>
+  )
+}
+
+export default Header
